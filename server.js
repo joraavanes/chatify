34a76b515b26291452bcbbf638d90a9e28e01fe0
joraavanes <jora_a@outlook.com>
2021-05-04 14:04:07 +0430

    Add basic socket.io setup on both server & client

--- server.js
+++ server.js
@@ -13,4 +13,12 @@ app.get('/', (req, res, next) => {
     res.sendFile('index.html');
 });
 
+io.on('connection', socket => {
+    console.log('A user has connected');
+
+    socket.on('disconnect', reason => {
+        console.log('A user has just left');
+    });
+});
+
 httpServer.listen(PORT, () => console.log(`Server is running: http://localhost:${PORT}`));
