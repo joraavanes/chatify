04b333cc90efa42707554fa9b46e45ae34fd5ad9
joraavanes <jora_a@outlook.com>
2021-05-05 11:31:08 +0430

    Implement webpack and babel for react app

---
+++ client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+__webpack_base_uri__ = 'http://localhost:8080';
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output:{
+        path: path.join(__dirname, 'public', 'js'),
+        filename: 'bundle.[hash].js'
+    },
+    module:{
+        rules:[
+            {
+                use: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js$/
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './assets/index.html',
+            filename: '../index.html'
+        })
+    ]
+};
