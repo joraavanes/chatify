46c71e5daae5abe6b590cd6b0886ff1844eb6979
joraavanes <jora_a@outlook.com>
2021-05-06 15:07:39 +0430

    Add components on react app and apply them on react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Switch, BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import Chat from './components/Chat'
+import Join from './components/Join'
 
 const App = () => {
     return (
         <div>
             <h1>Chatify</h1>
             <Router>
+                <NavLink to="/">Join</NavLink>
+                <br/>
+                <NavLink to="/chat">Chat</NavLink>
                 <Switch>
-                    <Route path="/"/>
-                    <Route path="chat"/>
+                    <Route path="/" component={Join} exact/>
+                    <Route path="/chat" component={Chat} exact/>
                 </Switch>
             </Router>
         </div>
---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Join = () => {
+    return (
+        <div>
+            Join a chat room
+        </div>
+    )
+}
+
+export default Join
