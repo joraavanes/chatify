9a21e5f3757969beb7d6c559b8c3189555b7da29
joraavanes <jora_a@outlook.com>
2021-05-07 18:58:28 +0430

    Add querystring pkg, Emit userInfo using socketIO on Chat component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import SocketIO from 'socket.io-client'
+import qs from 'querystring'
+
+let socket;
 
 const Chat = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        socket = new SocketIO();
+
+        const userInfo = qs.parse(location.search.slice(1));
+        socket.emit('newUser', userInfo);
+
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
     return (
         <div>
             Chat
