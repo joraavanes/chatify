03252b440f16c6fbd564b451d87c6e6e85f03179
joraavanes <jora_a@outlook.com>
2021-05-14 23:54:58 +0430

    Add helpers basic layout

---
+++ helpers/message.js
@@ -0,0 +1,2 @@
+
+export const generateMessage = ({name, message, room}) => {};
---
+++ helpers/user.js
@@ -0,0 +1,8 @@
+let users = [];
+
+export const addUser = ({id, name, room}) => {};
+
+export const removeUser = (id) => {};
+
+
+
