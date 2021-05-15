f90174f6292bdf0bf6ef4cfa3a17ca62697f9471
joraavanes <jora_a@outlook.com>
2021-05-15 18:52:23 +0430

    Add message event listener on chat component - store incoming msg in the state

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import SocketIO from 'socket.io-client'
 import qs from 'querystring'
@@ -8,16 +8,29 @@ let socket;
 const Chat = () => {
     const location = useLocation();
 
+    const [messages, setMessages] = useState([]);
+
     useEffect(() => {
         socket = new SocketIO();
 
         const userInfo = qs.parse(location.search.slice(1));
         socket.emit('join', userInfo);
 
+        // Listens the message event from others
+        socket.on('message', msgStr => {
+            console.log(msgStr);
+            setMessages([...messages, msgStr]);
+        });
+
         return () => {
             socket.disconnect();
         }
     }, []);
+
+    useEffect(() => {
+
+
+    }, [messages]);
 
     return (
         <div>
