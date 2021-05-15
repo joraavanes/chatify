00fcd664eb108bbcc15357dfe5dd2c8a722594e7
joraavanes <jora_a@outlook.com>
2021-05-15 18:32:01 +0430

    Store joined user in the user array and complete join event

--- helpers/user.js
+++ helpers/user.js
@@ -1,8 +1,27 @@
 let users = [];
 
-export const addUser = ({id, name, room}) => {};
+const addUser = ({id, name, room}) => {
+    name = name.trim().toLowerCase().replace(' ', '-');
+    room = room.trim().toLowerCase().replace(' ', '-');
 
-export const removeUser = (id) => {};
+    const isUserExists = users.some(user => user.name == name && user.room == room);
 
+    if(isUserExists){
+        name = `name-${Math.round(Math.random() * 100)}`;
+    }
 
+    users.push({id, name, room});
+    console.log(users);
+};
 
+const removeUser = (id) => {
+    users = users.filter(user => user.id !== id);
+};
+
+const getCurrentUsersInRoom = room => users.filter(user => user.room == room);
+
+module.exports = {
+    addUser,
+    removeUser,
+    getCurrentUsersInRoom
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const { addUser, removeUser } = require('./helpers/user');
 
 const app = require('express')();
 const httpServer = require('http').createServer(app);
@@ -16,12 +17,18 @@ app.use((req, res, next) => {
 io.on('connection', socket => {
     console.log('A user has connected');
 
-    socket.on('join', data => {
-        console.log(socket.id, data);
+    socket.on('join', ({name, room}) => {
+        console.log(socket.id, name, room);
+        addUser({id: socket.id, name, room});
+
+        socket.broadcast.to(room).emit('message', `A new friend has joined, Welcome ${name}`);
+        socket.join(room);
+
     });
 
     socket.on('disconnect', reason => {
         console.log('A user has just left');
+        removeUser(socket.id);
     });
 });
 
