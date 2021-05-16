d233a997c2b4c35f23fc9b3e3b86123a03ee2bbb
joraavanes <jora_a@outlook.com>
2021-05-16 15:33:54 +0430

    Add userInfo in Chat component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -8,17 +8,20 @@ let socket;
 const Chat = () => {
     const location = useLocation();
 
+    const [userInfo, setUserInfo] = useState({});
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         socket = new SocketIO();
 
         const userInfo = qs.parse(location.search.slice(1));
+        setUserInfo(userInfo);
+
+        // Emits join event
         socket.emit('join', userInfo);
 
         // Listens the message event from others
         socket.on('message', msgStr => {
-            console.log(msgStr);
             // const newMessages = messages.concat(msgStr);
             setMessages(arr => [...arr, msgStr]);
         });
@@ -26,7 +29,7 @@ const Chat = () => {
         return () => {
             socket.disconnect();
         }
-    }, []);
+    }, [location.search]);
 
     useEffect(() => {
 
@@ -38,9 +41,6 @@ const Chat = () => {
             {messages && messages.map(message => (
                 <p>{message}</p>
             ))}
-            {/* <pre>
-                {messages}
-            </pre> */}
         </div>
     )
 }
