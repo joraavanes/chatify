22272a4cda0f977a7a72bfbc25410e31ed88e3f6
joraavanes <jora_a@outlook.com>
2021-05-19 23:59:13 +0430

    Implement basic layout to Chat component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -2,8 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import SocketIO from 'socket.io-client'
 import qs from 'querystring'
+import styled from 'styled-components'
 import MessageForm from './MessageForm';
 import Sidebar from './Sidebar';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100vh;
+`;
+
+const HorizontalContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 80vh;
+`;
 
 let socket;
 
@@ -44,15 +58,17 @@ const Chat = () => {
     }, [roomData]);
 
     return (
-        <div>
+        <Container>
             <Sidebar roomData={roomData}/>
-            {messages && messages.map(data => (
-                <p key={data.message}><b>{data.name}</b> says: {data.message}</p>
-            ))}
-            {socket &&
-                <MessageForm socket={socket}/>
-            }
-        </div>
+            <HorizontalContainer>
+                {messages && messages.map(data => (
+                    <p key={data.message}><b>{data.name}</b> says: {data.message}</p>
+                ))}
+                {socket &&
+                    <MessageForm socket={socket}/>
+                }
+            </HorizontalContainer>
+        </Container>
     )
 }
 
