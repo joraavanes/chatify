90cf9cfcb95b70680fac44abb7ebb58504ef983b
joraavanes <jora_a@outlook.com>
2021-06-09 11:42:25 +0430

    Add auto scrolling on messages box

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import VerticalContainer from './styled/VerticalContainer.styled'
 import Message from './Message'
 
 const Messages = ({messages}) => {
+    const $messagesContainer = useRef(null);
+
+    useEffect(() => {
+        
+        // Scroll down to the last message, using scrollHeight value
+        $messagesContainer.current.scrollTo({ 
+            top: $messagesContainer.current.scrollHeight,
+            behavior: 'smooth'
+        });
+
+    }, [messages]);
+
     return (
-        <VerticalContainer height="calc(100vh - 60px)" padding="10px" justifyContent="flex-start" overflowY="scroll">
+        <VerticalContainer ref={$messagesContainer} height="calc(100vh - 60px)" padding="10px" justifyContent="flex-start" overflowY="scroll">
             {messages && messages.map(data => (
                 // <p key={data.message}><b>{data.name}</b> : {data.message}</p>
                 <Message key={data.messageId} data={data}/>
