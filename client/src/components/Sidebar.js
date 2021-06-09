72cba56a60be9b8bd94420b2ee57d2c7207b5ef2
joraavanes <jora_a@outlook.com>
2021-06-09 13:58:18 +0430

    Add RoomInfo component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react'
+import RoomInfo from './styled/RoomInfo.styled';
 import UserInfo from './styled/UserInfo.styled';
 import VerticalContainer from './styled/VerticalContainer.styled'
 
@@ -12,8 +13,11 @@ const Sidebar = ({roomData}) => {
     }, [roomData]);
 
     return (
-        <VerticalContainer width="20vw" height="100vh" justifyContent="flex-start" backgroundColor="#8f91f5">
-            <h3>{roomData.room} <span>{roomData.users && roomData.users.length} roommate(s)</span></h3>
+        <VerticalContainer width="20vw" height="100vh" justifyContent="flex-start" backgroundColor="#8f91f5" boxShadow="10px 0 30px -9px #ccc">
+            <RoomInfo>
+                <h2 style={{padding: '4px 0', margin: 0}}>{roomData.room}</h2>
+                <span>{roomData.users && roomData.users.length} people</span>
+            </RoomInfo>
             {roomData.users && roomData.users.map(user => (
                 <UserInfo key={user.id}>
                     <span>{user.name}</span>
---
+++ client/src/components/styled/RoomInfo.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export default styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #7d81ef;
+    background-color: #a4f0e4;
+    border-radius: 30px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    padding: 0 0 4px;
+    margin: 0 0 8px;
+`;
