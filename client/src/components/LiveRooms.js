3cd5106b6eb133454cfe0d57003aa38040ea5ec2
joraavanes <jora_a@outlook.com>
2021-07-14 11:08:00 +0430

    Add useSelector to LiveRooms and receive rooms from the store

--- client/src/components/LiveRooms.js
+++ client/src/components/LiveRooms.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Container from './styled/Container.styled'
 import H3 from './styled/h3.styled'
 import LiveRoomsListStyled from './styled/LiveRoomsList.styled'
 import LiveRoomsItemStyled from './styled/LiveRoomItem.styled';
 import NoRoomMessageStyled from './styled/NoRoomMessage.styled';
 
-const LiveRooms = ({liveRooms}) => {
+const LiveRooms = ({}) => {
+    const liveRooms = useSelector(state => state.room.rooms);
+
     return (
         <>
             <H3 color="#fff">Or choose from live rooms</H3>
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import roomReducer from './roomReducer';
 
 export default combineReducers({
-    roomReducer
+    room: roomReducer
 });
